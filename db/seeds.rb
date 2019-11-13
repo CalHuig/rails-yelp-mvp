3241d680a8e95a7ff944fcd1c040778ddc8c8b57
Corentin Huillard <corentin.huillard@gmail.com>
2019-11-13 16:57:28 +0100

    created extraordinary responsive website with astonishing design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Melt',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '+33 1 76 46 26 34',
+    category:     'french'
+  },
+  {
+    name:         'Les delices de Pekin',
+    address:      '124 rue Saint Maur',
+    phone_number: '+33 3 34 57 68 10',
+    category:     'chinese'
+  },
+  {
+    name:         'Sushi Soba',
+    address:      '98 rue legendre',
+    phone_number: '+33 9 67 46 33 22',
+    category:     'japanese'
+  },
+  {
+    name:         'Lucky Luciano',
+    address:      '30 rue des Dames',
+    phone_number: '+33 5 77 88 99 31',
+    category:     'italian'
+  },
+  {
+    name:         'Dédé la frite',
+    address:      '2 place de la Bourse',
+    phone_number: '+33 7 63 55 11 20',
+    category:     'belgian'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
